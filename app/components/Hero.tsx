35168591e763d4a11a9d59ca02a0234153638b05
Chandra Sekhar <chandrasekhar4321n@gmail.com>
2026-05-10 21:11:18 +0530

    added animations

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,29 +1,69 @@
+"use client";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+const text =
+  "I build software that simplify and automate real world business operations using";
+
+const words = text.match(/\b\w+\b/g) ?? [];
 
 export default function Hero() {
   return (
-    <div className="flex relative ">
-      <div
-        className="bg-cyan-50 w-700 h-screen overflow-hidden flex justify-center items-center">
-        <div
-          className="flex justify-center pl-24 items-center ">
-          <p
-            className="font-semibold  text-neutral-700 font-sans text-7xl ">
-            I build software that simplify and automate real world business operations using
-            <span className="font-serif font-normal ">Full Stack ()</span> .
-          </p>
-        </div>
+    <section className="relative w-full h-screen bg-cyan-50 overflow-hidden">
+
+      {/* TEXT */}
+      <div className="absolute inset-0 z-20 flex items-center">
+        <motion.div  
+          initial ={
+            { x:-70 }
+          }
+          animate ={{x :0 }}
+          transition={ {duration : 1.5}}
+         className="max-w-6xl pl-24 flex flex-wrap gap-x-3 gap-y-2">
+          {words.map((word: string, index: number) => (
+            <span
+              key={index}
+              className="
+                text-7xl
+                font-semibold
+                text-neutral-700
+                hover:text-black
+                transition-all
+                duration-300
+                hover:cursor-grab
+              "
+            >
+              {word}
+            </span>
+          ))}
+
+          <span
+            className="
+              text-7xl
+              font-serif
+              font-semibold
+              text-neutral-700 
+              hover:text-black 
+               hover:cursor-grab
+            "
+          >
+            Full Stack ()
+          </span>
+
+        </motion.div>
       </div>
-      <div
-        className="bg-cyan-50 w-500 h-screen  pl-40 overflow-hidden flex justify-end-safe  items-end ">
-           <Image
-           src="/profile.webp"
-           alt="user profile image"
-           width={800}
-           height={500}>
-           </Image>
+
+      {/* IMAGE */}
+      <div className="absolute bottom-0 right-0 z-10">
+        <Image
+          src="/profile.webp"
+          alt="profile"
+          width={600}
+          height={500}
+          className="object-contain"
+          priority
+        />
       </div>
-  
-    </div>
+
+    </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,47 @@
-import Image from "next/image";
+"use client";
+
 import Hero from "./components/Hero";
 import { About } from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="bg-cyan-50 w-screen min-h-screen relative text-black">
 
-      {/* Centered Floating Name */}
-     <div className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
-  <span
-    className="
-      px-6 py-2 text-3xl font-serif tracking-widest  rounded-2xl
-      bg-white/30 backdrop-blur-md border border-white/40
-      transition-all duration-300 
-      hover:bg-white/40 hover:shadow-lg
-      cursor-pointer
-    "
-  >
-    CHANDRA SEKHAR
-  </span>
-</div>
+      {/* Floating Name */}
+      <div className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
+        <motion.div
+          initial={{  y: -30 }}
+          animate={{  y : 0  }}
+          transition={{
+            duration: 0.5,
+          }}
+          whileHover={{ scale: 1.1 }}
+          className="
+            inline-block
+            px-6 py-2
+            text-3xl
+            font-serif
+            tracking-widest
+            rounded-2xl
+            bg-white/30
+            backdrop-blur-md
+            border border-white/40
+            hover:bg-white/40
+            hover:shadow-lg
+            cursor-pointer
+          "
+        >
+          CHANDRA SEKHAR
+        </motion.div>
+      </div>
+
       <Hero />
       <About />
-      <Projects/>
+      <Skills />
+      <Projects />
     </div>
   );
 }
